a38b10de6f405d626dac29c0312d6f51c5932581
MinecraftDawn <eric4902077@gmail.com>
2018-08-15 23:28:47 +0800

    onDisable

--- src/amplificationBackpack/AmplificationBackpack.java
+++ src/amplificationBackpack/AmplificationBackpack.java
@@ -4,6 +4,7 @@ import amplificationBackpack.Events.InventoryCloseEvent;
 import amplificationBackpack.commands.CmdAmplificationBackpack;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AmplificationBackpack extends JavaPlugin {
@@ -11,5 +12,12 @@ public class AmplificationBackpack extends JavaPlugin {
     public void onEnable() {
         Bukkit.getPluginCommand("bp").setExecutor((CommandExecutor) new CmdAmplificationBackpack());
         Bukkit.getPluginManager().registerEvents(new InventoryCloseEvent(),this);
+    }
+
+    @Override
+    public void onDisable() {
+//        for(Player p :Bukkit.getOnlinePlayers()){
+//
+//        }
     }
 }
