b651670d3f104a284ae0e4e8633301cb9525a233
MinecraftDawn <eric4902077@gmail.com>
2018-08-17 16:33:23 +0800

    Modify Backpack.yml writing mode

--- src/amplificationBackpack/commands/CmdAmplificationBackpack.java
+++ src/amplificationBackpack/commands/CmdAmplificationBackpack.java
@@ -2,10 +2,12 @@ package amplificationBackpack.commands;
 
 import amplificationBackpack.file.BackpackManager;
 import amplificationBackpack.file.YMLManager;
+import com.meowj.langutils.lang.LanguageHelper;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class CmdAmplificationBackpack implements CommandExecutor {
     @Override
@@ -15,6 +17,9 @@ public class CmdAmplificationBackpack implements CommandExecutor {
 
         if (args.length == 0) {
             p.openInventory(BackpackManager.getBackpack(p));
+        } else if (args.length == 1){
+            ItemStack item = p.getItemInHand();
+            p.sendMessage(LanguageHelper.getItemDisplayName(item,"zh_tw"));
         }
         return false;
     }
--- src/amplificationBackpack/file/BackpackManager.java
+++ src/amplificationBackpack/file/BackpackManager.java
@@ -1,17 +1,22 @@
 package amplificationBackpack.file;
 
+import com.meowj.langutils.lang.LanguageHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class BackpackManager {
 
     private static YMLManager yml = YMLManager.getInstance();
 
     private static YamlConfiguration data = yml.data;
-
 
 
     public static void setBackpack(Player p, Inventory inv) {
@@ -23,32 +28,52 @@ public class BackpackManager {
         for (int i = 0; i < inv.getSize(); i++) {
             item = inv.getItem(i);
 
+            Map<Enchantment, Integer> ench = item.getEnchantments();
+            List<String> enchName = new ArrayList<>();
+
             data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "Item"), SwitchItemStr.Item2Str(item));
-            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "Name"), "");
-            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "耐久"), "");
+
+            if (item == null){
+                data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "Name"),null);
+                data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "數量"),null);
+                data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "耐久"),null);
+                data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "附魔"), enchName);
+                continue;
+            }
+
+            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "Name"), LanguageHelper.getItemDisplayName(item, "zh_tw"));
+            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "數量"), item.getAmount());
+            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "耐久"), item.getType().getMaxDurability());
+
+            for (Map.Entry<Enchantment, Integer> e : ench.entrySet()) {
+                enchName.add(LanguageHelper.getEnchantmentDisplayName(e, "zh_tw"));
+            }
+            data.set(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "附魔"), enchName);
         }
 
         yml.saveData();
     }
 
     public static Inventory getBackpack(Player p) {
-        if(! data.isSet(p.getUniqueId().toString())){
-            Inventory inv = Bukkit.createInventory(null,54,"擴增背包");
+
+        if (!data.isSet(p.getUniqueId().toString())) {
+            Inventory inv = Bukkit.createInventory(null, 54, "擴增背包");
 
             return inv;
         }
 
-        Inventory inv = Bukkit.createInventory(null,54,"擴增背包");
+        Inventory inv = Bukkit.createInventory(null, 54, "擴增背包");
 
-        int size = data.getInt(yml.str2DotStr(p.getUniqueId().toString(),"格數"));
+        int size = data.getInt(yml.str2DotStr(p.getUniqueId().toString(), "格數"));
 
         String str;
 
         for (int i = 0; i < size; i++) {
             str = data.getString(yml.str2DotStr(p.getUniqueId().toString(), "物品", Integer.toString(i), "Item"));
-            inv.setItem(i,SwitchItemStr.Str2Item(str));
+            inv.setItem(i, SwitchItemStr.Str2Item(str));
         }
 
         return inv;
     }
+
 }
